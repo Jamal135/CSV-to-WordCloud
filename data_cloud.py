d79986afd4d565aaaf0d08c1287e3c4086012a3b
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-01-18 05:59:18 +1000

    Implemented word correction.

--- data_cloud.py
+++ data_cloud.py
@@ -12,34 +12,36 @@ import os
 LOCATION = os.path.dirname(os.path.abspath(__file__)) + "\\data_folder\\%s"
 
 
-def read_csv(input_name, columns, only_adjectives, strict):
-    ''' Returns: Loaded Data List from CSV File. '''
-    df = pd.read_csv(LOCATION % input_name + ".csv", keep_default_na=False,
-                     usecols=columns, low_memory=True)
-    return [clean_data(val, only_adjectives, strict)
-            for val in df.astype(str).values.tolist()]
-
-
 def get_adjectives(text: str, strict: bool):
     ''' Returns: String Cut to Relevant Adjective Data. '''
-    blob = TextBlob(text) # Corpora: python -m textblob.download_corpora
+    blob = TextBlob(text)  # Corpora: python -m textblob.download_corpora
     if strict:
         return [word for (word, tag) in blob.tags if tag == ("JJ")]
     else:
         return [word for (word, tag) in blob.tags if tag.startswith("JJ")]
 
 
-def clean_data(input_list: str, only_adjectives: bool, strict: bool):
+def clean_data(input_list: str, adjectives: bool, strict: bool, correct: bool):
     ''' Returns: Cleaned and Lowered List Data. '''
     for index, _ in enumerate(input_list):
         input_list[index] = re.sub(r"[^\S ]+", "", input_list[index])
         input_list[index] = re.sub(r"[^\w ]", "", input_list[index])
         input_list[index] = ' '.join(input_list[index].split())
         input_list[index] = input_list[index].lower()
-        if only_adjectives:
+        if correct:
+            input_list[index] = str(TextBlob(input_list[index]).correct())
+        if adjectives:
             input_list[index] = " ".join(
                 get_adjectives(input_list[index], strict))
     return input_list
+
+
+def read_data(input_name, columns, only_adjectives, strict, correct):
+    ''' Returns: Loaded Data List from CSV File. '''
+    df = pd.read_csv(LOCATION % input_name + ".csv", keep_default_na=False,
+                     usecols=columns, low_memory=True)
+    return [clean_data(val, only_adjectives, strict, correct)
+            for val in df.astype(str).values.tolist()]
 
 
 def gen_wordcloud(df_string, words):
@@ -50,10 +52,10 @@ def gen_wordcloud(df_string, words):
 
 
 def gen_cloud(input_name: str, output_name: str, columns: list,
-              only_adjectives: bool = True, strict: bool = False,
-              words: int = 5000):
+              adjectives: bool = True, strict: bool = False,
+              correct: bool = True, words: int = 5000):
     ''' Returns: Built Word Cloud and Cleaned + Frequency Data. '''
-    data_list = read_csv(input_name, columns, only_adjectives, strict)
+    data_list = read_data(input_name, columns, adjectives, strict, correct)
     df_string = " ".join(list(itertools.chain.from_iterable(data_list)))
     wordcloud = gen_wordcloud(df_string, words)
     wordcloud.to_file('output_folder/' + output_name + '.png')
@@ -64,4 +66,4 @@ def gen_cloud(input_name: str, output_name: str, columns: list,
     frequency_df.to_csv('output_folder/' + output_name + '-frequency.csv')
 
 
-gen_cloud("Reviews-Data", "cloud-image", ["Review", "Title"], strict = True)
+gen_cloud("Reviews-Data", "cloud-image", ["Review", "Title"])
